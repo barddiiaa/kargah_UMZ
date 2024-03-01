e39a9a3c9285d254cacca38bbfd26b3f8b118821
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-01 05:55:07 -0800

    Add files via upload

---
+++ print value.py
@@ -0,0 +1,18 @@
+# name = input("name: ")
+# city = input("city: ")
+# print("salam", name, "az", city)
+
+# a = 10
+# print( type(a) )
+
+# b = 3.14
+# print( type(b) )
+
+# c = True
+# print( type(c) )
+
+# v =" bardia"
+# print( type(v) )
+
+ciyy = float( input("name: ") )
+print( type(ciyy) )
