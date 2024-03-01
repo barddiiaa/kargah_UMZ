ef1601f26b930c753be4f2826fa82247f14a79a4
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-01 06:30:25 -0800

    ezafe kardn turtle

---
+++ tamrin turtle.py
@@ -0,0 +1,16 @@
+import turtle
+
+paper = turtle.Screen()
+medad = turtle.Turtle()
+
+size =int( turtle.textinput("size", "andaze tool zele vard konid:") ) 
+# sakht moraba
+medad.forward(size)
+medad.left(90)
+medad.forward(size)
+medad.left(90)
+medad.forward(size)
+medad.left(90)
+medad.forward(size)
+medad.left(90)
+turtle.done()
