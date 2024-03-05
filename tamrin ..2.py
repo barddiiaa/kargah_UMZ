18a11575b42ab2bc334c474c074701aae47a72fa
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-03-05 23:48:54 +0330

    tamrin 2

---
+++ tamrin ..2.py
@@ -0,0 +1,13 @@
+while 1:
+    darkhast = input("darkhast:")
+    if darkhast == "khroj":
+        break
+    elif darkhast == "variz":
+        n = int(input("meghdar:"))
+        hesab += n
+    elif darkhast == "bardasht":
+        n = int(input("meghdar")) 
+        hesab -= n
+    elif darkhast == "mojodi":
+        print("mojodi",hesab)
+           
