9e19851ce1d607b12888ad19d99394275c17000e
barddiiaa <161323097+barddiiaa@users.noreply.github.com>
2024-04-01 20:39:46 +0330

    mamoriat aval

---
+++ 1.py
@@ -0,0 +1,10 @@
+import re
+def decrypt_clue(text):
+
+    pattern = r'\b(?:list|int|def|class|return|try|except|for|from|import|global|if|else|lambda|nonlocal|pass|raise|while|with|and|or|assert|break|continue|finally)\b'
+
+    keywords = re.findall(pattern, text)
+
+    print("kalamat kelidy dar Phyton:")
+    for keyword in keywords:
+        print(keyword)
